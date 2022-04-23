ecb4411af3394c281e6f5f62a2c042c67890a403
Jacob McHugh <jakemchugh1@gmail.com>
2022-04-22 20:19:27 -0700

    added enemies and player death

--- Player.gd
+++ Player.gd
@@ -1,10 +1,17 @@
 extends KinematicBody
 
+signal hit
+
 export var speed = 14.0
 export var jump_impulse = 20.0
 export var fall_acceleration = 75.0
+export var bounce_impulse = 16.0
 
 var velocity = Vector3.ZERO
+
+func die():
+	emit_signal("hit")
+	queue_free()
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -30,3 +37,17 @@ func _physics_process(delta):
 	
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("mob"):
+			var mob = collision.collider
+			if Vector3.UP.dot(collision.normal) > 0.1:
+				mob.squash()
+				velocity.y = bounce_impulse
+
+
+
+
+func _on_MobDetector_body_entered(body):
+	die()
